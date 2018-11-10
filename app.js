2a819156610f0c896ceabf8b9da8f3e4161544f4
israr2018 <israr.sharepoint@gmail.com>
2018-11-10 01:24:38 -0800

    All code is commented out

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ var cors=require('cors');
 app.use(cors({credentials: true, origin: true}));
 
 // //const logger = require('morgan');
-var bodyParser=require('body-parser');
-var mongoose=require('mongoose');
+// var bodyParser=require('body-parser');
+// var mongoose=require('mongoose');
 // var db=mongoose.connect(config.database.connection).then(()=>{
 // console.log("Successfully connected to the database.")
 // },(error)=>{
 // console.log(`Could not connect to database something goes wrong:${error}`);
 // });
-var  CarAds=require('./models/CarAds');
-var  CarMakes=require('./models/CarMakes');
-var  CarModels=require('./models/CarModels');
-var  UserModel=require('./models/UserModel')
+// var  CarAds=require('./models/CarAds');
+// var  CarMakes=require('./models/CarMakes');
+// var  CarModels=require('./models/CarModels');
+// var  UserModel=require('./models/UserModel')
 
-app.use('/uploads',express.static('uploads'));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+// app.use('/uploads',express.static('uploads'));
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json());
 
-var carAdsRouter=require('./Routes/CarAdsRoutes')(CarAds);
-var carMakesRouter=require('./Routes/CarMakesRoutes')(CarMakes);
-var carModelsRouter=require('./Routes/CarModelsRoutes')(CarModels);
-var authRouter=require('./Routes/authRoute')(UserModel);
-app.use("/api/CarAds",carAdsRouter);
-app.use("/api/CarMakes",carMakesRouter);
-app.use("/api/CarModels",carModelsRouter);
-app.use("/api/authenticate",authRouter);
-const port=process.env.PORT||8080;
+// var carAdsRouter=require('./Routes/CarAdsRoutes')(CarAds);
+// var carMakesRouter=require('./Routes/CarMakesRoutes')(CarMakes);
+// var carModelsRouter=require('./Routes/CarModelsRoutes')(CarModels);
+// var authRouter=require('./Routes/authRoute')(UserModel);
+// app.use("/api/CarAds",carAdsRouter);
+// app.use("/api/CarMakes",carMakesRouter);
+// app.use("/api/CarModels",carModelsRouter);
+// app.use("/api/authenticate",authRouter);
+const port=process.env.PORT||3000;
 app.get('/',function(req,res) {
     res.send("Welcome to the MCZ Restfull  Web Services ");
 });
