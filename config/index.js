f66a59d04c274bbc124f1c34127d7bcd73d6650f
israr2018 <israr.sharepoint@gmail.com>
2018-11-10 00:45:09 -0800

    Testing the app.js

--- config/index.js
+++ config/index.js
@@ -5,11 +5,4 @@ const node_env=(process.env.NODE_ENV||'development').trim();
 // //var dev=(process.env.NODE_ENV||'development');
 
 const config = require('./env/'+node_env);
-
-//const config={};
-
-// merge the default , the current one ie production  ,development and export it for use
-//module.exports = _.merge({}, defaults, config);
-
-
 module.exports=config;
