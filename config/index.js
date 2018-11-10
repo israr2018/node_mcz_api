b63ea668b1e1c04f67f4ea7d2752a342c449ed65
israr2018 <israr.sharepoint@gmail.com>
2018-11-10 00:26:11 -0800

    test2

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
 const dotenv=require('dotenv');
 //var _ = require("lodash");
  const defaults =require("./env/defaults");
-// const node_env=(process.env.NODE_ENV||'development').trim();
+const node_env=(process.env.NODE_ENV||'development').trim();
 // //var dev=(process.env.NODE_ENV||'development');
 
-// const config = require('./env/'+node_env);
+//const config = require('./env/'+node_env);
+
+const config={};
 
 // merge the default , the current one ie production  ,development and export it for use
 //module.exports = _.merge({}, defaults, config);
