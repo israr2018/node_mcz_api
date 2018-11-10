1c152789fcebf8001dc0e0578b8d114ec6ab642c
israr2018 <israr.sharepoint@gmail.com>
2018-11-10 00:20:18 -0800

    Test

--- config/index.js
+++ config/index.js
@@ -1,12 +1,13 @@
-var dotenv=require('dotenv');
+const dotenv=require('dotenv');
 //var _ = require("lodash");
-var defaults =require("./env/defaults");
-var node_env=(process.env.NODE_ENV||'development').trim();
-//var dev=(process.env.NODE_ENV||'development');
+ const defaults =require("./env/defaults");
+// const node_env=(process.env.NODE_ENV||'development').trim();
+// //var dev=(process.env.NODE_ENV||'development');
 
-var config = require('./env/'+node_env);
-config.port=process.env.port;
+// const config = require('./env/'+node_env);
+
 // merge the default , the current one ie production  ,development and export it for use
 //module.exports = _.merge({}, defaults, config);
+
 
 module.exports=config;
